5bdddfa9cb9e871b51addcf510743c61eb29233d
OmarEldeeb98 <52624232+OmarEldeeb98@users.noreply.github.com>
2024-08-20 15:27:19 +0300

    Add files via upload

---
+++ objc.m
@@ -0,0 +1,130 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "LCAppInfo.h"
+
+@implementation LCAppInfo
+- (instancetype)initWithBundlePath:(NSString*)bundlePath {
+	 self = [super init];
+	 if(self) {
+        _bundlePath = bundlePath;
+        _info = [NSMutableDictionary dictionaryWithContentsOfFile:[NSString stringWithFormat:@"%@/Info.plist", bundlePath]];
+    }
+    return self;
+}
+
+- (NSString*)displayName {
+    if (_info[@"CFBundleDisplayName"]) {
+        return _info[@"CFBundleDisplayName"];
+    } else if (_info[@"CFBundleName"]) {
+        return _info[@"CFBundleName"];
+    } else if (_info[@"CFBundleExecutable"]) {
+        return _info[@"CFBundleExecutable"];
+    } else {
+        return nil;
+    }
+}
+
+- (NSString*)version {
+    NSString* version = _info[@"CFBundleShortVersionString"];
+    if (!version) {
+        version = _info[@"CFBundleVersion"];
+    }
+    return version;
+}
+
+- (NSString*)bundleIdentifier {
+    return _info[@"CFBundleIdentifier"];
+}
+
+- (NSString*)dataUUID {
+    if (!_info[@"LCDataUUID"]) {
+        self.dataUUID = NSUUID.UUID.UUIDString;
+    }
+    return _info[@"LCDataUUID"];
+}
+
+- (NSString*)tweakFolder {
+    return _info[@"LCTweakFolder"];
+}
+
+- (void)setDataUUID:(NSString *)uuid {
+    _info[@"LCDataUUID"] = uuid;
+    [self save];
+}
+
+- (void)setTweakFolder:(NSString *)tweakFolder {
+    _info[@"LCTweakFolder"] = tweakFolder;
+    [self save];
+}
+
+- (NSString*)bundlePath {
+    return _bundlePath;
+}
+
+- (NSMutableDictionary*)info {
+    return _info;
+}
+
+- (UIImage*)icon {
+    UIImage* icon = [UIImage imageNamed:[_info valueForKeyPath:@"CFBundleIcons.CFBundlePrimaryIcon.CFBundleIconFiles"][0] inBundle:[[NSBundle alloc] initWithPath: _bundlePath] compatibleWithTraitCollection:nil];
+    if(!icon) {
+        icon = [UIImage imageNamed:@"DefaultIcon"];
+    }
+    return icon;
+}
+
+- (UIImage *)generateLiveContainerWrappedIcon {
+    UIImage *icon = self.icon;
+    if (![NSUserDefaults.standardUserDefaults boolForKey:@"LCFrameShortcutIcons"]) {
+        return icon;
+    }
+
+    UIImage *lcIcon = [UIImage imageNamed:@"AppIcon76x76"];
+    CGFloat iconXY = (lcIcon.size.width - 40) / 2;
+    UIGraphicsBeginImageContextWithOptions(lcIcon.size, NO, 0.0);
+    [lcIcon drawInRect:CGRectMake(0, 0, lcIcon.size.width, lcIcon.size.height)];
+    CGRect rect = CGRectMake(iconXY, iconXY, 40, 40);
+    [[UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:7] addClip];
+    [icon drawInRect:rect];
+    UIImage *newIcon = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newIcon;
+}
+
+- (NSDictionary *)generateWebClipConfig {
+    NSDictionary *payload = @{
+        @"FullScreen": @YES,
+        @"Icon": UIImagePNGRepresentation(self.generateLiveContainerWrappedIcon),
+        @"IgnoreManifestScope": @YES,
+        @"IsRemovable": @YES,
+        @"Label": self.displayName,
+        @"PayloadDescription": [NSString stringWithFormat:@"Web Clip for launching %@ (%@) in LiveContainer", self.displayName, self.bundlePath.lastPathComponent],
+        @"PayloadDisplayName": self.displayName,
+        @"PayloadIdentifier": self.bundleIdentifier,
+        @"PayloadType": @"com.apple.webClip.managed",
+        @"PayloadUUID": self.dataUUID,
+        @"PayloadVersion": @(1),
+        @"Precomposed": @NO,
+        @"toPayloadOrganization": @"LiveContainer",
+        @"URL": [NSString stringWithFormat:@"livecontainer://livecontainer-launch?bundle-name=%@", self.bundlePath.lastPathComponent]
+    };
+    return @{
+        @"ConsentText": @{
+            @"default": [NSString stringWithFormat:@"This profile installs a web clip which opens %@ (%@) in LiveContainer", self.displayName, self.bundlePath.lastPathComponent]
+        },
+        @"PayloadContent": @[payload],
+        @"PayloadDescription": payload[@"PayloadDescription"],
+        @"PayloadDisplayName": self.displayName,
+        @"PayloadIdentifier": self.bundleIdentifier,
+        @"PayloadOrganization": @"LiveContainer",
+        @"PayloadRemovalDisallowed": @(NO),
+        @"PayloadType": @"Configuration",
+        @"PayloadUUID": @"345097fb-d4f7-4a34-ab90-2e3f1ad62eed",
+        @"PayloadVersion": @(1),
+    };
+}
+
+- (void)save {
+    [_info writeToFile:[NSString stringWithFormat:@"%@/Info.plist", _bundlePath] atomically:YES];
+}
+@end
